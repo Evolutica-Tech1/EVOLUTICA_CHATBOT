036d8904b4a66822cdb32e629c403f8e6f5f426c
nicolazarte2894 <nicolazarte2894@gmail.com>
2023-06-02 21:30:41 -0300

    chatbot openai ok

---
+++ openaichat.py
@@ -0,0 +1,23 @@
+import openai
+from config import API_KEY
+openai.api_key = API_KEY
+conversation = ""
+i = 1
+
+while (i != 0):
+    question = input("User: ")
+    conversation += "\nUser: " + question + "\nAI: "
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=conversation,
+        temperature=0.5,
+        max_tokens=100,
+        top_p=1.0,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+        stop=["\n", "User: ", "AI: "]
+    )
+    answer = response.choices[0].text.strip()
+    # print(answer)
+    conversation += answer
+    print("AI: " + answer + "\n")

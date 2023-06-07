62e150eb01ff234ad3f534b242281483dce9f924
nicolazarte2894 <nicolazarte2894@gmail.com>
2023-06-07 20:21:55 -0300

    Test entrenamiento con csv Sigue funcionando mal las respuestas. Ver los hiperparámetros

--- chat.py
+++ chat.py
@@ -5,12 +5,12 @@ openai.api_key = API_KEY
 
 def get_response(message):
     response = openai.Completion.create(
-        model="davinci:ft-personal:model-with-csv-data-v1-0-2023-06-07-22-15-31",
+        model="davinci:ft-personal:model-with-csv-data-2023-06-07-21-27-59",
         prompt=message,
-        temperature=0.2,
+        temperature=0.5,
         max_tokens=100,
         top_p=1.0,
-        frequency_penalty=0.8,
+        frequency_penalty=0.5,
         presence_penalty=0.0,
         # stop=["\n", "User: ", "AI: "]
     )
